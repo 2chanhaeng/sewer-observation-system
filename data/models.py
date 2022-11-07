80ccc16ca37632e7fe19a2f201af08e929e0a243
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-07 13:57:24 +0900

    Create Sewage model
    Sewer 모델에서 하수관 정보와 하수량 정보 모델을 분리하여 Sewage 모델 생성

--- data/models.py
+++ data/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 
 class Sewer(models.Model):
-    IDN = models.CharField(max_length=8, verbose_name="고유번호")
-    GUBN = models.CharField(max_length=4, verbose_name="구분코드")
-    GUBN_NAM = models.CharField(max_length=4, verbose_name="구분명")
-    MEA_YMD = models.DateTimeField(verbose_name="측정일자")
-    MEA_WAL = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="측정수위")
-    SIG_STA = models.CharField(max_length=8, verbose_name="통신상태")
-    REMARK = models.CharField(max_length=255, verbose_name="위치정보")
+    idn = models.CharField(max_length=8, verbose_name="고유번호")
+    gubn = models.CharField(max_length=4, verbose_name="구분코드")
+    gubn_nam = models.CharField(max_length=4, verbose_name="구분명")
+    remark = models.CharField(max_length=255, verbose_name="위치정보")
 
     def __str__(self):
-        return f"{self.IDN} {self.MEA_YMD}"
+        return f"Sewer {self.idn} ({self.remark})"
+
+
+class Sewage(models.Model):
+    sewer = models.ForeignKey(Sewer, on_delete=models.CASCADE)
+    mea_ymd = models.DateTimeField(verbose_name="측정일자")
+    mea_wal = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="측정수위")
+    sig_sta = models.CharField(max_length=8, verbose_name="통신상태")
+
+    def __str__(self):
+        return f"Sewage data of {self.sewer.idn} at {self.mea_ymd}"
