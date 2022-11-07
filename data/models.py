6d1a7f0930298a1bdade1dc1545ac6ad3a47bed2
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-07 13:48:50 +0900

    Create Sewer model
    `IDN`: 고유번호
    `GUBN`: 구분코드
    `GUBN_NAM`: 구분명
    `MEA_YMD`: 측정일자
    `MEA_WAL`: 측정수위
    `SIG_STA`: 통신상태
    `REMARK`: 위치정보

--- data/models.py
+++ data/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Sewer(models.Model):
+    IDN = models.CharField(max_length=8, verbose_name="고유번호")
+    GUBN = models.CharField(max_length=4, verbose_name="구분코드")
+    GUBN_NAM = models.CharField(max_length=4, verbose_name="구분명")
+    MEA_YMD = models.DateTimeField(verbose_name="측정일자")
+    MEA_WAL = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="측정수위")
+    SIG_STA = models.CharField(max_length=8, verbose_name="통신상태")
+    REMARK = models.CharField(max_length=255, verbose_name="위치정보")
+
+    def __str__(self):
+        return f"{self.IDN} {self.MEA_YMD}"
